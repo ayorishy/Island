d4ba2cc7aedce379abba91469c9117e4c4e0fe3c
1nonly <91359128+Inon1y@users.noreply.github.com>
2023-05-30 11:15:02 +0800

    对接后端 QQ

--- sidebar.php
+++ sidebar.php
@@ -3,29 +3,20 @@
 		<h4 class="poppin_300" _msttexthash="3991715" _msthash="22">社交</h4>
 		<hr>
 		<ul>
-			<li class="icon_tw">
-				<a href="#" onclick="window.open(this.href, 'TWwindow', 'width=650, height=450, menubar=no, toolbar=no, scrollbars=yes'); return false;">
+			<li>
+				<a href="<?php $this->options->linkqq(); ?>" onclick="window.open(this.href, 'TWwindow', 'width=650, height=450, menubar=no, toolbar=no, scrollbars=yes'); return false;">
 					<div class="icon">
-						<img class="def pc" src="https://blog.nonly.cn/usr/themes/Island/assets/img/social/icon_tw.png" alt="twitter" width="20" height="16" _mstalt="1985711" _msthash="25">
-						<img class="hvr pc" src="https://blog.nonly.cn/usr/themes/Island/assets/img/social/icon_tw_hvr.png" alt="twitter" width="20" height="16" _mstalt="1985711" _msthash="23">
+						<img class="def pc" src="https://blog.ban.moe/usr/themes/Island/assets/img/social/qq.png" alt="qq" width="25" height="25" _mstalt="1985711" _msthash="25">
+						<img class="hvr pc" src="https://blog.ban.moe/usr/themes/Island/assets/img/social/qq_hover.png" alt="qq" width="25" height="25" _mstalt="1985711" _msthash="23">
 					</div>
 				</a>
 			</li>
-			<!--<li class="icon_fb">-->
-			<!--	<a href="#" onclick="window.open(this.href, 'FBwindow', 'width=650, height=450, menubar=no, toolbar=no, scrollbars=yes'); return false;">-->
+			
+			<!--<li>-->
+			<!--	<a href="#" onclick="window.open(this.href, 'TWwindow', 'width=650, height=450, menubar=no, toolbar=no, scrollbars=yes'); return false;">-->
 			<!--		<div class="icon">-->
-			<!--			<img class="def pc" src="http://never-Island.com/cms/wp-content/themes/Island/images/common/icon_fb.png" alt="facebook" width="15" height="20" _mstalt="5097560" _msthash="28">-->
-			<!--			<img class="hvr pc" src="http://never-Island.com/cms/wp-content/themes/Island/images/common/icon_fb_hvr.png" alt="facebook" width="15" height="20" _mstalt="5097560" _msthash="26">-->
-			<!--			<img class="sp scl" src="http://never-Island.com/cms/wp-content/themes/Island/images/common_sp/icon_fb.png" alt="facebook" width="15" height="20" _msthidden="A" _mstalt="113646" _msthash="27">-->
-			<!--		</div>-->
-			<!--	</a>-->
-			<!--</li>-->
-			<!--<li class="icon_ln">-->
-			<!--	<a href="#" target="_blank">-->
-			<!--		<div class="icon">-->
-			<!--			<img class="def pc" src="http://never-Island.com/cms/wp-content/themes/Island/images/common/icon_ln.png" alt="line" width="21" height="20" _mstalt="2952677" _msthash="31">-->
-			<!--			<img class="hvr pc" src="http://never-Island.com/cms/wp-content/themes/Island/images/common/icon_ln_hvr.png" alt="line" width="21" height="20" _mstalt="2952677" _msthash="29">-->
-			<!--			<img class="sp scl" src="http://never-Island.com/cms/wp-content/themes/Island/images/common_sp/icon_ln.png" alt="line" width="21" height="20" _msthidden="A" _mstalt="46748" _msthash="30">-->
+			<!--			<img class="def pc" src="https://blog.nonly.cn/usr/themes/Island/assets/img/social/qq.png" alt="qq" width="30" height="30" _mstalt="1985711" _msthash="25">-->
+			<!--			<img class="hvr pc" src="https://blog.nonly.cn/usr/themes/Island/assets/img/social/qq_hover.png" alt="qq" width="30" height="30" _mstalt="1985711" _msthash="23">-->
 			<!--		</div>-->
 			<!--	</a>-->
 			<!--</li>-->
